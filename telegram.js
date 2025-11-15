87c35334cdacde028157474a3af0be65a714c808
TheChefMamii <123protitanik@gmail.com>
2025-11-15 22:55:50 +0300

    Update Telegram bot configuration and messages
    Updated the bot token and chat ID, and modified alert messages.

--- telegram.js
+++ telegram.js
@@ -1,13 +1,13 @@
 // telegram.js
 
 // BURAYA KENDİ BOT TOKEN ve CHAT ID'ni YAPIŞTIR
-const BOT_TOKEN = 'BOT_TOKEN_BURAYA'; 
-const CHAT_ID = 'CHAT_ID_BURAYA';
+const BOT_TOKEN = '8387694074:AAHF30x-1NcmE0Gs4v2jWMpyJPDuwy0XCa4'; 
+const CHAT_ID = '6750266187';
 
 document.getElementById('sendMsg').onclick = () => {
   const msg = document.getElementById('messageText').value.trim();
   if(!msg) {
-    alert('Lütfen bir mesaj yazın ❤️');
+    alert('Ne söylemek istersin güzelimm❤️');
     return;
   }
     
@@ -16,16 +16,17 @@ document.getElementById('sendMsg').onclick = () => {
   fetch(url, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ chat_id: CHAT_ID, text: `❤️ Webden Mesaj:\n\n${msg}` })
+    body: JSON.stringify({ chat_id: CHAT_ID, text: `❤️ Evrenin en güzel kızından mesaj:\n\n${msg}` })
   })
   .then(r => r.json())
   .then(data => {
     if(data.ok) {
-      alert('Mesajın gönderildi ❤️');
+      alert('Mesajın gitti');
       document.getElementById('messageText').value = '';
     } else {
-      alert('Bir hata oluştu. Lütfen tekrar deneyin.');
+      alert('Bir hata oluştu.');
     }
   })
   .catch(() => alert('Bağlantı hatası. Lütfen internet bağlantınızı kontrol edin.'));
+
 };
